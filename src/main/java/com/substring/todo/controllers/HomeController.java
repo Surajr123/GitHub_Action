c5e934e96cab641326aad7941b8dbd8b5fa6ee0f
Surajr123 <surajrXworkz@gmail.com>
2026-08-26 17:24:58 +0530

    TO_DO App for githbub

---
+++ src/main/java/com/substring/todo/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.substring.todo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@CrossOrigin(origins = "*")
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/api/v1/status")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> status() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "OK");
+        response.put("message", "Todo Backend API is running successfully");
+        response.put("service", "todo-backend");
+        return ResponseEntity.ok(response);
+    }
+}
